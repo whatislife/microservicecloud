8342515613cddc09723f4754460577f40864de3b
whatislife <1053400192@qq.com>
2020-06-15 21:49:00 +0800

    注册中心7001、7002、7003 服务8001访问1.2.3修改完毕

---
+++ microservicecloud-eureka-7002/src/main/java/com/atguigu/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author frnakman
+ * @create 2020-06-15 13:40
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7002_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class,args);
+    }
+}
