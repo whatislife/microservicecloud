b6b28308060cd5d0d4f0113866dfbb78045e99dc
whatislife <1053400192@qq.com>
2020-06-15 23:02:15 +0800

    Ribbon初步配置

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,6 @@
-package com.atguigu.springcloud;
+package com.atguigu.springcloud.cfgbeans;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,10 @@ import org.springframework.web.client.RestTemplate;
  */
 @Configuration
 public class ConfigBean {
+    //boot -->spring   applicationContext.xml
+    // --- @Configuration配置   ConfigBean = applicationContext.xml
     @Bean
+    @LoadBalanced  //Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端       负载均衡的工具。
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -1,4 +1,4 @@
-package com.atguigu.springcloud.cfgbeans.controller;
+package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +14,13 @@ import java.util.List;
 @RestController
 public class DeptController_Consumer {
 
-    private static final String REST_URL_PREFIX = "http://localhost:8001";
+    //private static final String REST_URL_PREFIX = "http://localhost:8001";
+    private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
+    /**
+     *
+     * 使用 使用restTemplate访问restful接口非常的简单粗暴无脑。 (url, requestMap,
+     * ResponseBean.class)这三个参数分别代表 REST请求地址、请求参数、HTTP响应转换被转换成的对象类型。
+     */
     @Autowired
     private RestTemplate restTemplate;
 
