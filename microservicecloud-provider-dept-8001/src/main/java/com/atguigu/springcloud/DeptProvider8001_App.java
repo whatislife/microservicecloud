a862cff511ca0f7673b773a1a5e5f91bbbb02360
whatislife <1053400192@qq.com>
2020-06-15 14:02:18 +0800

    provider-8001 提交EurekaClient

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -2,12 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author frnakman
  * @create 2020-06-15 10:15
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class, args);
