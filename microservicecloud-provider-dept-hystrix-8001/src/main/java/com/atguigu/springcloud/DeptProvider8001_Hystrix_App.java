236a9705574bae1955abb39b5ea8cb916f5e41cf
whatislife <1053400192@qq.com>
2020-06-16 19:07:49 +0800

    Hystrix熔断机制代码

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_Hystrix_App.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author frnakman
+ * @create 2020-06-15 10:15
+ */
+@SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient //服务发现
+@EnableCircuitBreaker//对hystrixR熔断机制的支持
+public class DeptProvider8001_Hystrix_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider8001_Hystrix_App.class, args);
+    }
+
+}
