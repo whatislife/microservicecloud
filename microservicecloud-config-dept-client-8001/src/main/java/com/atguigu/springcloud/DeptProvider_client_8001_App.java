9c64983cc0adf7c2faaac22622247f522f3f93ac
whatislife <1053400192@qq.com>
2020-06-17 22:33:05 +0800

    jar运行提交

--- microservicecloud-config-dept-client-8001/src/main/java/com/atguigu/springcloud/DeptProvider_client_8001_App.java
+++ microservicecloud-config-dept-client-8001/src/main/java/com/atguigu/springcloud/DeptProvider_client_8001_App.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 @EnableDiscoveryClient //服务发现
+//jar运行代码  java -jar microservicecloud.jar --spring.cloud.config.profile=dev
 public class DeptProvider_client_8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_client_8001_App.class, args);
